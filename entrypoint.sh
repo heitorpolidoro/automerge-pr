4702e63b9bf27da47dc76955c156f3c5214a0256
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-11-24 16:42:26 -0300

    autocreate-pr #1

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,13 @@ BOLD='\x1b[1m'
 NORMAL="\x1b[0m"
 
 set -e
-if [[ -n "$INPUT_ALLOWED_USERS" ]] && [[ ! " ${INPUT_ALLOWED_USERS[*]} " =~ " ${GITHUB_ACTOR} " ]]
+token=$(eval echo "\$$GITHUB_ACTOR")
+if [[ -z "$token" ]]
 then
   echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto-release"
 else
   echo "::group::GitHub authentication"
-    echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
+    echo "$token" | gh auth login --with-token
   echo "::endgroup::"
 
   echo "::group::Configuring to auto merge"
