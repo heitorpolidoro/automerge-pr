62d91aeec53ce9cffef7ebf5084c36539c11ae89
Heitor Polidoro <14806300+heitorpolidoro@users.noreply.github.com>
2021-11-24 17:46:49 -0300

    autocreate-pr (#4)
    * autocreate-pr #1
    
    * debug 1
    
    * automerge 1
    
    * debug 1
    
    * debug 2
    
    * debug 3
    
    * debug 4
    
    * automerge
    
    * automerge --squash
    
    * debug 1
    
    * git checkout
    
    * git checkout 2
    
    * git checkout 3
    
    * git checkout 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,17 @@ BOLD='\x1b[1m'
 NORMAL="\x1b[0m"
 
 set -e
-if [[ -n "$INPUT_ALLOWED_USERS" ]] && [[ ! " ${INPUT_ALLOWED_USERS[*]} " =~ " ${GITHUB_ACTOR} " ]]
+token=$(eval echo "\$$GITHUB_ACTOR")
+if [[ -z "$token" ]]
 then
-  echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto-release"
+  echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto merge"
 else
   echo "::group::GitHub authentication"
-    echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
+    echo "$token" | gh auth login --with-token
+    gh auth status
   echo "::endgroup::"
-
+git status
   echo "::group::Configuring to auto merge"
+    gh pr merge --auto --squash
   echo "::endgroup::"
 fi
